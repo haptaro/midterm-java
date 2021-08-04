889d045c42dd794d3c3fc6a5c53f4b4337eb5288
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-03 20:28:22 -0700

    :art: add Test

--- src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
+++ src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
@@ -40,4 +40,15 @@ class PeakAndValleyCalculatorTest {
         int[] expectedPeak = new int[] {6, 6, 6};
         assertTrue(Arrays.equals(peakAndValley.getPeak(), expectedPeak));
     }
+
+    @Test
+    void test_technical_report_1() {
+        PeakAndValleyCalculator calculator = new PeakAndValleyCalculator();
+        int[] input = new int[] {2, 6, 6, 6, 3};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {2};
+        assertTrue(Arrays.equals(peakAndValley.getValley(), expectedValley));
+        int[] expectedPeak = new int[] {6, 6, 6};
+        assertTrue(Arrays.equals(peakAndValley.getPeak(), expectedPeak));
+    }
 }
