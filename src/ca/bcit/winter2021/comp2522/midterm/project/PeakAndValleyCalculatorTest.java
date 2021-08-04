e026ef141abf29b6ac02afc50200e0662c1715ae
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-03 20:41:21 -0700

    :art: add Test

--- src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
+++ src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
@@ -13,16 +13,6 @@ class PeakAndValleyCalculatorTest {
     }
 
     @Test
-    void test_calcurate_part1() {
-        int[] input = new int[] {6, 1, 4};
-        PeakAndValley peakAndValley = calculator.calcurate(input);
-        int[] expectedValleys = new int[] {1};
-        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValleys));
-        int[] expectedPeak = new int[] {6};
-        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
-    }
-
-    @Test
     void test_technical_report_1() {
         int[] input = new int[] {2, 6, 6, 6, 3};
         PeakAndValley peakAndValley = calculator.calcurate(input);
@@ -45,5 +35,25 @@ class PeakAndValleyCalculatorTest {
     @Test
     void test_technical_report_3() {
         // Should ask when input value is empty
+    }
+
+    @Test
+    void test_technical_report_4() {
+        int[] input = new int[] {5,5,5,5};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {5,5,5,5};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {5,5,5,5};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
+    }
+
+    @Test
+    void test_technical_report_5() {
+        int[] input = new int[] {6, 1, 4};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {1};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {6};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
     }
 }
