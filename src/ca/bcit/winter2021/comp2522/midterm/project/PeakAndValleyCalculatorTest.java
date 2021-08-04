5904a54a37c8b207f62a34573e4cf2989b637bb3
Kotaro Fukuo <haptaro.dev@gmail.com>
2021-08-03 20:44:38 -0700

    :art: add Tests

--- src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
+++ src/ca/bcit/winter2021/comp2522/midterm/project/PeakAndValleyCalculatorTest.java
@@ -56,4 +56,44 @@ class PeakAndValleyCalculatorTest {
         int[] expectedPeak = new int[] {6};
         assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
     }
+
+    @Test
+    void test_technical_report_6() {
+        int[] input = new int[] {5, 6666, 2222, 3333, 2222, 1};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {1};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {6666};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
+    }
+
+    @Test
+    void test_technical_report_7() {
+        int[] input = new int[] {1, 99999};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {1};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {99999};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
+    }
+
+    @Test
+    void test_technical_report_8() {
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {1};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {9};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
+    }
+
+    @Test
+    void test_technical_report_9() {
+        int[] input = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        PeakAndValley peakAndValley = calculator.calcurate(input);
+        int[] expectedValley = new int[] {1};
+        assertTrue(Arrays.equals(peakAndValley.getValleys(), expectedValley));
+        int[] expectedPeak = new int[] {9};
+        assertTrue(Arrays.equals(peakAndValley.getPeaks(), expectedPeak));
+    }
 }
